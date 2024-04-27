2a4328bff2712e929c232aa1fd8e2e11d76922a1
Emmanuel <hahyordeji@gmail.com>
2024-04-27 03:29:04 +0100

    Worked on mobile menu

--- app/(components)/topnav.tsx
+++ app/(components)/topnav.tsx
@@ -3,9 +3,13 @@ import Image from "next/image";
 import logo from "../../public/assets/logo.svg";
 import { FaBars } from "react-icons/fa6";
 import Link from "next/link";
-import { ButtonOutline } from "@/components/ui/button";
+import { Button, ButtonOutline } from "@/components/ui/button";
 import { usePathname } from "next/navigation";
+import { useState } from "react";
+import { Underline } from "./events";
+import { IoCloseCircleOutline, IoCloseOutline } from "react-icons/io5";
 const TopNav = () => {
+  const [showMenu, setShowMenu] = useState<boolean>(false);
   const pathname = usePathname();
   const navLinks = [
     { link: "Home", path: "/" },
@@ -24,22 +28,68 @@ const TopNav = () => {
         alt="logo"
         className="size-auto"
       />
-      <nav className="flex items-center max-lg:hidden gap-x-[48px] text-[16px] leading-[19.2px] text-[#050505] mt-[18.5px] ">
+      {/* Links */}
+      <nav
+        className={`flex  items-center lg:gap-x-[48px] text-[16px] leading-[19.2px] text-[#050505] lg:mt-[18.5px] transition-all duration-500 ${
+          showMenu
+            ? "fixed flex flex-col items-start top-0 right-0 bg-white max-lg:translate-x-0 h-screen w-[204px] px-[16px] gap-y-[8px]"
+            : "max-lg:translate-x-[200%] max-lg:fixed max-lg:flex-col max-lg:items-start top-0 right-0 max-lg:bg-white max-lg:translate- max-lg:h-screen max-lg:w-[204px] max-lg:px-[16px] max-lg:gap-y-[8px] "
+        }`}
+      >
+        {/* Close icon */}
+        {/* {showMenu && ( */}
+        <>
+          <div
+            onClick={() => setShowMenu(false)}
+            className="absolute left-[-46px] top-[80px]  bg-gradient-to-tr from-primaryRed to-primaryOrange py-[2px] pl-[2px] rounded-tl-[12px] rounded-bl-[12px]"
+          >
+            <div className="py-[8px] bg-white px-[12px] rounded-tl-[12px] rounded-bl-[12px]">
+              <div className="rounded-full  bg-gradient-to-tr from-primaryRed to-primaryOrange p-[1px]">
+                <IoCloseOutline className="text-[18px] rounded-full bg-white" />
+              </div>
+            </div>
+          </div>
+          <Image
+            src={logo}
+            width={34.85}
+            height={39.99}
+            alt="logo"
+            className="mb-[24px] self-end mt-[16px] lg:hidden"
+          />
+        </>
+        {/* )} */}
         {navLinks.map((item, index) => (
           <Link
             href={item.path}
             key={index}
             className={`${
               pathname === item.path &&
-              "text-[#D7322C] font-[700] text-[18px] leading-[21.6px]"
-            }   text-[16px] text-[#050505]  leading-[19.2px]`}
+              "lg:text-[#D7322C] lg:font-[700] lg:text-[18px] leading-[21.6px]"
+            }   text-[16px] lg:text-[#050505]  leading-[19.2px] font-[500] text-black max-lg:text-start `}
           >
             {item.link}
+            {showMenu && (
+              <Underline
+                className={`${
+                  pathname !== item.path && "h-[0.8px] opacity-30 "
+                } h-[1px] mt-[12px]`}
+              />
+            )}
           </Link>
         ))}
+        {showMenu && (
+          <div className="mt-[24px] px-[16px] self-start">
+            <ButtonOutline className="py-[8px] px-[20px] text-[16px] leading-[19.2px] text-black h-fit">
+              Login
+            </ButtonOutline>
+          </div>
+        )}
       </nav>
       <div className="p-[22.12px]">
-        <FaBars className="text-black text-[18px] lg:hidden" />
+        <FaBars
+          onClick={() => setShowMenu((prev) => !prev)}
+          className="text-black text-[20px] lg:hidden"
+        />
         <div className="max-lg:hidden ">
           <ButtonOutline className="text-[14px] leading-[16.8px] text-black font-[600] h-fit px-[16px] py-[12px]">
             {pathname === "/" ? "Become a Member" : "Login"}
